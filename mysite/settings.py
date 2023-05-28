c4593ee92d9c1754085c7ff554f5e6c632704fe0
INJAMAMUL HAQ <61545224+injamamul786@users.noreply.github.com>
2023-05-29 02:34:40 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'chats',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +62,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
